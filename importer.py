04999d5341386d2d05f46243c6a5c4829a60b656
Martin Ellis <martin@ellis.name>
2016-08-09 22:59:19 +0100

    Index commits in elasticsearch.

--- importer.py
+++ importer.py
@@ -1,6 +1,19 @@
-import json
+import httplib
+import urllib
+from json import dumps
 
 class Importer:
 
+    def __init__(self):
+        self.conn = httplib.HTTPConnection('localhost', 9200)
+        self.conn.connect()
+
     def commit(self, commit):
-        print(json.dumps(commit, indent=2))
+        json = dumps(commit, indent=2)
+        headers = {
+            "Content-type": "application/json"
+        }
+        path = 'git/commits/' + commit['hash']
+        self.conn.request("PUT", path, json, headers)
+        resp = self.conn.getresponse()
+        resp.read()
